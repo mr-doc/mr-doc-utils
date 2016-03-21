00ac6d430f1d2c1d5a707327a79e75c5a536fe95
iwatakeshi <iwatakeshi@gmail.com>
2016-03-20 23:16:24 -0600

    Initial Commit
    Added files.

---
+++ test/option.js
@@ -0,0 +1,62 @@
+/* eslint-env node, mocha */
+const assert = require('chai').assert;
+const Option = require('../src/option');
+
+describe('option', () => {
+  it('should return helper methods', () => {
+    assert.isDefined((new Option().options()));
+  });
+  describe('option.parser', () => {
+    it('should return an object', () => {
+      assert.isObject(Option.parser());
+    });
+    it('should return an extended object', () => {
+      assert.deepEqual(Option.parser({
+        engine: 'babylon',
+      }), {
+        language: 'javascript',
+        engine: 'babylon',
+        version: '6',
+      });
+    });
+  });
+  describe('option.compiler', () => {
+    it('should return an object', () => {
+      assert.isObject(Option.compiler());
+    });
+    it('should return an extended object', () => {
+      assert.deepEqual(Option.compiler({
+        file: {
+          format: 'html',
+        },
+      }), {
+        file: {
+          name: 'files',
+          format: 'html',
+        },
+        template: {
+          path: null,
+          engine: 'jade',
+        },
+      });
+    });
+  });
+  describe('option.theme', () => {
+    it('should return an object', () => {
+      assert.isObject(Option.theme());
+    });
+    it('should return an extended object', () => {
+      assert.deepEqual(Option.theme({ path: '/test' }),
+      { name: 'mr-doc-theme-default', path: '/test' });
+    });
+  });
+  describe('option.project', () => {
+    it('should return an object', () => {
+      assert.isObject(Option.project());
+    });
+    it('should return an extended object', () => {
+      assert.deepEqual(Option.project({ name: 'Mr. Doc' }),
+      { name: 'Mr. Doc', homepage: '#', repository: '#' });
+    });
+  });
+});
