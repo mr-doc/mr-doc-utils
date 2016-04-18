5580fbacc3696ad402b7f2cc636c87e1f78b77a5
iwatakeshi <iwatakeshi@gmail.com>
2016-04-17 20:29:28 -0600

    :strawberry: Patch
    Added Output class to export.

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const Log = require('./src/log');
 const Source = require('./src/source');
 const Reference = require('./src/reference');
 const Doc = require('./src/doc');
+const Output = require('./src/output');
 module.exports = {
   Option,
   Log,
   Source,
   Reference,
   Doc,
+  Output,
 };
